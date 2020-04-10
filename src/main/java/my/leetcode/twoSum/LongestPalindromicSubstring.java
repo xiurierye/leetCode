7ff6a8859701ed7e8c5fc3d19b35f249995eff99
bbd <zhaoji1012@gmail.com>
2020-04-10 22:02:01 +0800

    LongestPalindromicSubstring solution2

--- src/main/java/my/leetcode/twoSum/LongestPalindromicSubstring.java
+++ src/main/java/my/leetcode/twoSum/LongestPalindromicSubstring.java
@@ -10,6 +10,67 @@ import java.util.Set;
  */
 public class LongestPalindromicSubstring {
 
+    public static class Solution1 {
+        public String longestPalindrome(String s) {
+            int length = s.length();
+            if (length == 0) {
+                return "";
+            }
+            int i,j;
+            Set<Palindromic> palindromics=new HashSet<>();
+
+            Palindromic maxPalindromic = new Palindromic(0,0);
+            for (i=0; i < length ;i++) {
+                char charI = s.charAt(i);
+                for ( j=i; j <length ; j++) {
+                    if (j+1>=length){
+                        break;
+                    }
+                    if (s.charAt(j+1)!=charI) {
+                        break;
+                    }
+                }
+                if ((i-1)<0 ||(j+1)==length){
+                    Palindromic current= new Palindromic(i, j);
+                    maxPalindromic = maxPalindromic.length()>= current.length()? maxPalindromic:current;
+                    continue;
+                }
+                int maxLength = Math.min(i , length - 1 - j);
+                int i1=0;
+                for (; i1 < maxLength; i1++) {
+                    char leftChar = s.charAt(i - 1 - i1);
+                    char rightChar = s.charAt(j + 1 + i1);
+                    if (leftChar==rightChar){
+                        continue;
+                    }
+                    else {
+                        break;
+                    }
+                }
+                Palindromic current = new Palindromic(i-i1, j+ i1);
+                maxPalindromic = maxPalindromic.length()>= current.length()? maxPalindromic:current;
+            }
+
+            return s.substring( maxPalindromic.i,maxPalindromic.j+1);
+        }
+
+        public static class Palindromic{
+            public final int i;
+            public final int j;
+
+            public Palindromic(int i, int j) {
+                this.i = i;
+                this.j = j;
+            }
+
+            public int length(){
+                return j-i+1;
+            }
+
+        }
+
+    }
+
     public static class Solution {
         public String longestPalindrome(String s) {
             int length = s.length();
