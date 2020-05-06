05348700a61a163a6ef44a9fb7e222a1012affe9
xiurierye <zhaoji004@paic.com.cn>
2020-05-06 08:13:12 +0800

    try Divide two integers

--- src/main/java/my/leetcode/DivideTwoIntegers.java
+++ src/main/java/my/leetcode/DivideTwoIntegers.java
@@ -3,48 +3,52 @@ package my.leetcode;
 public class DivideTwoIntegers implements Medium {
 
     static class Solution {
-        int absDividend ;
-        int absDivisor ;
+        int finalDividend;
+        int finalDivisor;
 
-        int prefix =1;
+        int prefix = 1;
 
         public int divide(int dividend, int divisor) {
             if (dividend == Integer.MIN_VALUE && divisor == 1) {
                 return Integer.MAX_VALUE;
             }
 
-            absDividend = Math.abs(dividend);
-            absDivisor = Math.abs(divisor);
-            if (absDividend < absDivisor) {
+            finalDividend = dividend > 0 ? (~dividend + 1) : dividend;
+            finalDivisor = divisor > 0 ? (~divisor + 1) : divisor;
+            if (finalDividend > finalDivisor) {
                 return 0;
             }
 
             int d1 = dividend & Integer.MIN_VALUE;
             int d2 = divisor & Integer.MIN_VALUE;
-            if (d1 == 0  ^  d2 ==0){
+            if (d1 == 0 ^ d2 == 0) {
                 prefix = -1;
             }
 
-            return  prefix* inner(absDividend,absDivisor);
+            final int result = inner(finalDividend, finalDivisor);
+            if (prefix == 1) {
+                return -result;
+            }
+            return result;
 
         }
 
-        private  int inner(int dividend, int divisor){
-            if (dividend < divisor) {
+        private int inner(int dividend, int divisor) {
+            if (dividend > divisor) {
                 return 0;
             }
 
 
-            int count = 1;
+            int count = -1;
             int suq = divisor;
             for (; ; ) {
-                int remain = Integer.MAX_VALUE - divisor;
-                if (remain < divisor) {
+                int remain = Integer.MIN_VALUE - divisor;
+                if (remain > divisor) {
                     break;
                 }
 
-                if (suq << 1 < dividend) {
-                    count++;
+                if (suq << 1 > dividend) {
+                    count--;
                     suq = suq << 1;
                 } else {
                     break;
@@ -54,7 +58,7 @@ public class DivideTwoIntegers implements Medium {
             int remain = dividend - suq;
 
 
-            return count   + this.inner(remain, divisor);
+            return count + this.inner(remain, divisor);
         }
     }
 
